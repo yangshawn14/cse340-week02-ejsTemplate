08b91efb5adc2f8571cf29967b11f2860fcba00d
Shawn Yang <yangshawn14@gmail.com>
2024-01-16 21:35:49 -0700

    add express layouts to require statements

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
