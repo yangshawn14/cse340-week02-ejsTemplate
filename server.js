b4e72a05f24d4eb17e18a46ada4b6c7eff66490b
Shawn Yang <yangshawn14@gmail.com>
2024-01-17 19:54:58 -0700

    Undoing previous test for quiz

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const static = require("./routes/static")
  *************************/
 app.use(static)
 // Index route
-app.get("/cars", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index", {title: "Home"})
 })
 
