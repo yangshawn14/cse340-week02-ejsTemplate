7c0c140e5b39894a8b51f4729d4ec338ef774cb7
Shawn Yang <yangshawn14@gmail.com>
2024-02-24 16:25:37 -0700

    Bring base controller into scope

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * Static Files
