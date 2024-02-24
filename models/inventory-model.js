7743991895f33778b9a67e698e8cedfe98fc72d7
Shawn Yang <yangshawn14@gmail.com>
2024-02-24 16:59:59 -0700

    Export function

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -25,4 +25,4 @@ async function getInventoryByClassificationId(classification_id) {
     }
 }
 
-module.exports = { getClassifications }
+module.exports = { getClassifications, getInventoryByClassificationId }
