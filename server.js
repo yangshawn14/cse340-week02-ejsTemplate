8e4cc3c87a916e6ddde184f6c9abd2b8bde452fd
Shawn Yang <yangshawn14@gmail.com>
2024-03-08 18:22:06 -0700

    Add messaging middleware

--- server.js
+++ server.js
@@ -37,6 +37,12 @@ app.use(session({
   name: 'sessionId',
 }))
 
+// Express Messages Middleware
+app.use(require("connect-flash")())
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res)
+  next()
+})
 /* ***********************
  * View Engine and Templates
  *************************/
